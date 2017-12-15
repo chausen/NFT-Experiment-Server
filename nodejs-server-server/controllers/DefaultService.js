74e3def64777fe3ce40bd7c5a10ceeed523f6a3f
chausen <clayhausen@gmail.com>
2017-12-14 19:45:23 -0500

    Added logic to write to DB

--- nodejs-server-server/controllers/DefaultService.js
+++ nodejs-server-server/controllers/DefaultService.js
@@ -9,15 +9,26 @@ exports.logPOST = function(args, res, next) {
    * no response value expected for this operation
    **/
 
-  console.log(`${args.entry.value.ec}
-               ${args.entry.value.voltage}
-               ${args.entry.value.pH}
-               ${args.entry.value.rH}
-               ${args.entry.value.wTemp}
-               ${args.entry.value.aTemp}
-               ${args.entry.value.timestamp}
-              `);
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://localhost:27017/GH";
+  let dataLogText = `${args.entry.value.ec}
+                     ${args.entry.value.voltage}
+                     ${args.entry.value.pH}
+                     ${args.entry.value.rH}
+                     ${args.entry.value.wTemp}
+                     ${args.entry.value.aTemp}
+                     ${args.entry.value.timestamp}`;                    
+  
 
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;    
+    db.collection("EnvParms").insertOne(args.entry.value, function(err, res) {
+      if (err) throw err;
+      console.log("Inserted:");
+      console.log(dataLogText);
+      db.close();
+    });
+  });  
   
   res.end();
 }
