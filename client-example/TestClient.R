6d13644463a30c335d8af8d2e4756bbfe903162b
chausen <clayhausen@gmail.com>
2018-01-05 07:44:15 -0500

    Updated R test client, moved it to client directory, and updated the gitignore

--- client-example/TestClient.R
+++ client-example/TestClient.R
@@ -1,7 +1,7 @@
 library(httr)
 
 timestamp <- "2016-12-07T13:30:25Z"
-basereq <- 'http://localhost:8080/log/'
+basereq <- "http://localhost:8080/log/"
 fullreq <- paste0( basereq, timestamp, "/" )
 
 res <- GET(fullreq)
