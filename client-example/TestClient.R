3d2076c19e7f2a531a362c4a2c70e4fbe4629b08
Emerick Larkin <emerickl@ufl.edu>
2018-01-10 21:02:02 -0500

    1. Removed unnecessary code in shiny server code. 2. Created a mock R data object. 3. Added leaflets for each sensor/data type.

--- client-example/TestClient.R
+++ client-example/TestClient.R
@@ -2,9 +2,11 @@ library(RJSONIO)
 
 timestamp <- "2016-12-07T13:30:25Z"
 basereq <- "http://localhost:8080/log/"
-fullreq <- paste0( basereq, timestamp, "/" )
+fullreq <- paste0(basereq, timestamp, "/" )
 
 logs <- fromJSON(fullreq)
 
 print(logs)
+
+logs
 
