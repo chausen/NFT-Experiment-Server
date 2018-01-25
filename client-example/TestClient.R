404388a1bfb12273d661b3892744c5e716b6d591
chausen <clayhausen@gmail.com>
2018-01-24 19:35:22 -0500

    Updating example R code to use the proper server address

--- client-example/TestClient.R
+++ client-example/TestClient.R
@@ -1,12 +1,9 @@
 library(RJSONIO)
 
 timestamp <- "2016-12-07T13:30:25Z"
-basereq <- "http://localhost:443/log/"
-fullreq <- paste0( basereq, timestamp, "/" )
+basereq <- "http://45.32.220.103:443/log/"
+fullreq <- paste0(basereq, timestamp, "/" )
 
 logs <- fromJSON(fullreq)
 
 print(logs)
-
-logs
-
